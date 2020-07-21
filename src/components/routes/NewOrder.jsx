9a4c6c2bfc98b79475e80ef1426f8a68bd7d2d2a
DavidSwanberg <davidswanberg767@gmail.com>
2020-07-20 20:56:33 -0400

    separate milkshake form

--- src/components/routes/NewOrder.jsx
+++ src/components/routes/NewOrder.jsx
@@ -1,82 +1,16 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Layout from '../shared/Layout';
 
-import OrderForm from "../shared/OrderForm";
-import Layout from "../shared/Layout";
-import apiUrl from "../apiConfig";
-
-const NewOrder = (props) => {
-   // console.log('ItemCreate props', props)
-  const [input, setInput] = useState({ flavor: "", holder: "", toppings: "", size: "", price:"" , paid:false });
-  const [item, setItem] = useState(null);
-
-const handleSelect = event => {
-  setInput({
-    ...input,
-    [event.target.name]: event.target.value
-  })
+const NewOrder = ()=>{
+    return(
+        <Layout>
+        <h4>What do you want to order?</h4>
+        <Link to="/order-icecream">Ice Cream</Link>
+        <Link to="/order-milkshake">Milkshake</Link>
+        </Layout>
+        
+    )
 }
-
-const handleSizeSelect = event =>{
-  if (event.target.value ==='small'){
-    setInput({
-      ...input,
-       size: 'small',
-       price: 5
-     });
-  }else if(event.target.value ==='medium'){
-    setInput({
-      ...input,
-       size: 'medium',
-       price: 6
-     });
-  }else{
-    setInput({
-      ...input,
-       size: 'large',
-       price: 7
-     });
-  }
-}
-
-  const handleChange = (event) => {
-    console.log("event", event.target.name, event.target.value);
-     setInput({
-      ...input,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log("handleSubmit",input);
-    axios({
-      url: `${apiUrl}/icecream`,
-      method: "POST",
-      data: input,
-    })
-      .then((res) => {
-          setItem({ createdItem: res.data.item })
-          props.history.push('/icecream')
-        })
-      .catch(console.error);
-  };
-
-  return (
-    <Layout>
-        <h4>New Order</h4>  
-        <OrderForm
-        order={input}
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-        handleSizeSelect={handleSizeSelect}
-        handleSelect={handleSelect}
-        setInput={setInput}
-        input={input}
-        cancelPath="/"
-        />
-    </Layout>
-  );
-};
 
 export default NewOrder;
