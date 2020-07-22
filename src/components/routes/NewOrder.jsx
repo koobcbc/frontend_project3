d6ba5d4758447e1aacc463917ad98dda141b7e79
DavidSwanberg <davidswanberg767@gmail.com>
2020-07-22 16:24:12 -0400

    changed font size

--- src/components/routes/NewOrder.jsx
+++ src/components/routes/NewOrder.jsx
@@ -6,9 +6,11 @@ const NewOrder = ()=>{
     return(
         <Layout>
             <div className="orderform">
+                <div className="choices">
                 <h4>What do you want to order?</h4>
                 <Link to="/order-icecream" className="btn btn-primary">Ice Cream</Link>
                 <Link to="/order-milkshake"className="btn btn-primary">Milkshake</Link>
+                </div>
             </div>
         </Layout>
         
